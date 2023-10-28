61171a98fda6dcf20a57d47daea46516e9a7dba3
Vladislav Kasiyan <weazy@internet.ru>
2023-10-28 16:15:03 +0300

    EF init
    Для описания сущностей, хранения класса DataContext для работы с данными

---
+++ EF/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        public DbSet<T> DbSet<T>() where T : class
+        {
+            return Set<T>();
+        }
+
+        public new IQueryable<T> Query<T>() where T : class
+        {
+            return Set<T>();
+        }
+    }
+}
