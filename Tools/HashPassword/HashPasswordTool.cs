59abe5a3d2c2f92ec5eda6f89d0d10fc3e6b4431
Vladislav Kasiyan <weazy@internet.ru>
2023-10-29 17:20:44 +0300

    Функция хеширования строки

---
+++ Tools/HashPassword/HashPasswordTool.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Tools.Encryption
+{
+    public static class HashPasswordTool
+    {
+        public static String HashPassword(this String password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                Byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                String hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
