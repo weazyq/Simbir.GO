4854fc755b23bd197c35b876bcd48a768bbe241f
Vladislav Kasiyan <weazy@internet.ru>
2023-10-30 23:29:53 +0300

    Исправление полей TransportEntity

--- Domain/Transports/Transport.cs
+++ Domain/Transports/Transport.cs
@@ -9,14 +9,14 @@
         public String Model { get; set; }
         public String Color { get; set; }
         public String Identifier { get; set; }
-        public String Description { get; set; }
+        public String? Description { get; set; }
         public Decimal Latitude { get; set; }
         public Decimal Longitude { get; set; }
-        public Decimal MinutePirce { get; set; }
-        public String DayPrice { get; set; }
+        public Decimal? MinutePirce { get; set; }
+        public Decimal? DayPrice { get; set; }
 
         public Transport(Int64 id, Int64 ownerId, Boolean canBeRented, TransportType transportType, String model, String color,
-            String identifier, String description, Decimal latitude, Decimal longitude, Decimal minutePirce, String dayPrice)
+            String identifier, String? description, Decimal latitude, Decimal longitude, Decimal? minutePirce, Decimal? dayPrice)
         {
             ID = id;
             OwnerId = ownerId;
--- EF/Entities/TransportEntity.cs
+++ EF/Entities/TransportEntity.cs
@@ -1,19 +1,17 @@
-﻿using Domain.Transports;
-
-namespace EF.Entities;
+﻿namespace EF.Entities;
 
 public class TransportEntity
 {
-    public Int64 ID { get; set; }
+    public Int64 Id { get; set; }
     public Int64 OwnerId { get; set; }
     public Boolean CanBeRented { get; set; }
-    public TransportType TransportType { get; set; }
+    public String TransportType { get; set; }
     public String Model { get; set; }
     public String Color { get; set; }
     public String Identifier { get; set; }
-    public String Description { get; set; }
+    public String? Description { get; set; }
     public Decimal Latitude { get; set; }
     public Decimal Longitude { get; set; }
-    public Decimal MinutePirce { get; set; }
-    public String DayPrice { get; set; }
+    public Decimal? MinutePrice { get; set; }
+    public Decimal? DayPrice { get; set; }
 }
