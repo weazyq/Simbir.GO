b47b70503ac597030389e16a5475dcc1d8cb1fb5
Vladislav Kasiyan <weazy@internet.ru>
2023-10-30 23:31:01 +0300

    Функция получения Enum элемента по строковому значению

---
+++ Tools/EnumHelper/EnumHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Tools.EnumHelper;
+
+public static class EnumHelper
+{
+    public static T GenEnum<T>(string value) where T : Enum
+    {
+        if (Enum.IsDefined(typeof(T), value))
+        {
+            return (T)Enum.Parse(typeof(T), value);
+        }
+
+        throw new ArgumentException($"Значение {value} не является допустимым значением перечисления {typeof(T).FullName}");
+    }
+}
